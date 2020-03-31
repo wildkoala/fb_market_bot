cd320ce618b19bf39ab965773f145de129935bbb
Twiggy Gordon <31520629+wildkoala@users.noreply.github.com>
2020-03-31 08:58:36 -0400

    Add the starting code template

---
+++ fb_market_reader.py
@@ -0,0 +1,12 @@
+# ultimate goal
+
+# read fb marketplace listing
+# check if the listing is 20% lower than the average
+	# of the last 5 of them sold on ebay (shipping + cost)
+# send me a text saying that we've got a potential flip:
+	# include the name of the item
+	# price they are asking
+	# average price on ebay
+# send a message to the seller offering them the Ebay average * .75
+
+
