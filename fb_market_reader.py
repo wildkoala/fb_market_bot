462fe14a7748062169c7e338a99d43442f9312d9
Twiggy Gordon <31520629+wildkoala@users.noreply.github.com>
2020-03-31 09:28:59 -0400

    Structure of the bot is mostly complete
    I can start implementing things/writing code at this point.

--- fb_market_reader.py
+++ fb_market_reader.py
@@ -1,5 +1,6 @@
-# ultimate goal
+# FLIP SOURCING
 
+# INTERACTION WITH FB MARKETPLACE
 # read fb marketplace listing
 # check if the listing is 20% lower than the average
 	# of the last 5 of them sold on ebay (shipping + cost)
@@ -9,4 +10,45 @@
 	# average price on ebay
 # send a message to the seller offering them the Ebay average * .75
 
+# I can make this thing more aggressive by sending a message
+# To every listing offering them the 75% of ebay price no matter what they listed it for.
+
+
+
+# INTERACTION WITH EBAY	
+# go to ebay
+# login to Ebay
+# search for the item keyword
+	# take the past 5 listings that have a condition of NOT "Parts Only"
+	# Get the price it sold for
+	# Get the cost of shipping
+
+
+# SENDING MESSAGES TO ME
+# setup a gmail account for this
+# login to gmail
+# send myself an email 
+	# the title is the listed item
+	# Then include the a message with what the item is
+	# What it has been selling for on average on ebay
+	# What it's being offered for on fb marketplace
+	# Send the amount difference and percent margin in "()"
+	# How many minutes away they are
+	# Display the ebay listings that it averaged at the bottom
+	
+===========================================================================
+
+# MAKING A SALE
+# Login to ebay
+# Post the item for 90% of the average sale price
+# Put up the listing
+
+# STORING RESULTS
+# Get the price that I bought it for on fb marketplace
+# Get the price it sold for on Ebay
+# Write results to a file
+	# Go to the next available line
+	# write in the name of item, bought for, sold for, margin, etc.
+	
+	
 
